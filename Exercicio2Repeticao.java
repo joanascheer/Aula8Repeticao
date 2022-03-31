9a34355c08508735190ca765022d5ba6a76e3830
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-03-31 15:46:54 -0300

    Update Exercicio2Repeticao.java
    Adicionando mais duas formas de fazer.

--- Exercicio2Repeticao.java
+++ Exercicio2Repeticao.java
@@ -7,10 +7,28 @@ public class Exercicio2Repeticao {
         System.out.println("Que número você quer multiplicar? ");
         int numero = leitor.nextInt();
 
+        /*
         for (int i=0; i <= 10; i++ ) {
             int resultado = numero * i;
             System.out.println(numero+ "x" +i+ "=" +resultado);
         }
+        */
+
+        /*
+        int i = 0;
+        while (i <= 10) {
+            int resultado = numero * i;
+            System.out.println(numero+ " x " +i+ " = " +resultado);
+            i++;
+        }
+        */
+
+        int i = 0;
+        do {
+            int resultado = numero * i;
+            System.out.println(numero+ " x " +i+ " = " +resultado);
+            i++;
+        } while(i <= 10);
     }
     
 }
